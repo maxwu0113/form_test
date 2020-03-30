5529d43d80c990041fa858241a9d9adc8b6062ca
EXOSOME(Lab501) <EXOSOME@DESKTOP-DOCNIH5>
2020-03-30 15:55:24 +0800

    還原檔案

--- fuckoffform/include/ahm_ext.h
+++
@@ -1,9 +0,0 @@
-#ifndef AHM_EXT_H
-#define AHM_EXT_H
-#include "ahm.h"
-
-AHM_API ahm::HardwareModel * theHardwareModelEx(iop::string szEx);
-AHM_API ahm::HardwareModel * theHardwareModelLAS();
-
-#endif
-
